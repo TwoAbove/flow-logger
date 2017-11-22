2546b254f33c8eea8eb70d40f02a7101760a7033
Seva Maltsev <sevamaltsev@gmail.com>
2017-11-22 19:41:07 +0300

    Added extra logging functions and log output cleaner
    Added error and message log functions
    
    Made log output clean from huge buffer output and cleaned errors to regular objects

--- test/index.js
+++ test/index.js
@@ -10,23 +10,52 @@ describe('Logger', async () => {
 
     const logger = new Logger();
     logger.timed('test1', 'first test');
-    expect(+logger.logged.test1.startedAt).to.equal(+now);
-    expect(logger.logged.test1.comment).to.equal('first test');
+    expect(+logger.test1.startedAt).to.equal(+now);
+    expect(logger.test1.comment).to.equal('first test');
     clock.tick(500);
-    logger.logged.test1.end();
-    expect(+logger.logged.test1.endedAt).to.equal(+now + 500);
+    logger.test1.end();
+    expect(+logger.test1.endedAt).to.equal(+now + 500);
     clock.restore();
+  });
+
+  it('Should correctly show message', () => {
+    const clock = sinon.useFakeTimers();
+    const now = Date.now();
+
+    const logger = new Logger();
+    logger.message('test', 'comment');
+    expect(logger.toString()).to.equal('{"test":{"comment":"comment","time":0}}');
+  });
+
+  it('Should correctly show error', () => {
+    const clock = sinon.useFakeTimers();
+    const now = Date.now();
+
+    const logger = new Logger();
+    const e = new Error('bla');
+    e.stack = 'at Bla';
+    logger.error(e);
+    expect(logger.toString()).to.equal('{"errored":true,"error":{"stack":"at Bla","message":"bla"},"erroredAt":0}');
   });
 
   it('Should correctly stringify logged', () => {
     const clock = sinon.useFakeTimers();
     const now = Date.now();
 
-    const logger = new Logger();
+    const logger = new Logger('test');
     logger.timed('test1', 'first test');
     clock.tick(500);
-    logger.logged.test1.end();
-    expect(logger.toString()).to.equal('{"test1":{"comment":"first test","startedAt":0,"endedAt":500,"duration":500}}');
+    logger.test1.end();
+    expect(logger.toString()).to.equal('{"name":"test","test1":{"comment":"first test","startedAt":0,"endedAt":500,"duration":500}}');
+    clock.restore();
+  });
+
+  it('Should change Buffer to base64', () => {
+    const clock = sinon.useFakeTimers();
+    const logger = new Logger();
+
+    logger.message('test', Buffer.from('1'));
+    expect(logger.toString()).to.equal('{"test":{"comment":"MQ==","time":0}}');
     clock.restore();
   });
 });
